4be2691e2fc9db6c370a833fd26ebdd9ef2767db
Mauro <salgadom7503@gmail.com>
2024-02-22 10:36:19 +0000

    Updates

---
+++ assets/x-rUk6_tNE.js
@@ -0,0 +1,6 @@
+import{r as a,N as l,h as d,_ as n}from"./index-rSRR66Z-.js";import{c as s}from"./createLucideIcon-yuH5cBOX.js";const p=a.forwardRef((r,t)=>{var o;const{container:e=globalThis==null||(o=globalThis.document)===null||o===void 0?void 0:o.body,...c}=r;return e?l.createPortal(a.createElement(d.div,n({},c,{ref:t})),e):null});/**
+ * @license lucide-react v0.321.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const $=s("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{p as $,$ as X};
