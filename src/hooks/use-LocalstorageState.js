45d346e8f6433b6bce9b7f73ada1b8752ec5347b
Mauro <salgadom7503@gmail.com>
2024-03-10 00:39:53 +0000

    n

--- src/hooks/use-LocalstorageState.js
+++ src/hooks/use-LocalstorageState.js
@@ -1,9 +1,11 @@
-import { useCallback, useMemo, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 const useLocalstorageState = (keyName, initialState) => {
-    const keyName_ = useMemo(() => JSON.stringify(keyName), [keyName])
+    const isMountedRef = useRef()
 
-    const [state, setState] = useState(() => {
+    const keyName_ = JSON.stringify(keyName)
+
+    const fetchData = useCallback(() => {
         const data = localStorage.getItem(keyName_)
         if(!data) {
             const initialState_ = typeof initialState === 'function' ? initialState() : initialState
@@ -11,13 +13,25 @@ const useLocalstorageState = (keyName, initialState) => {
             return initialState_
         }
         return JSON.parse(data)
-    })
+    }, [keyName_])
+
+    const [state, setState] = useState(fetchData)
 
     const setState_ = useCallback((newState) => {
         let newState_ = typeof newState === 'function' ? newState(state) : newState
         setState(newState_)
         localStorage.setItem(keyName_, JSON.stringify(newState_))
     }, [state, keyName_])
+
+    useEffect(() => {
+        if(isMountedRef.current) {
+            setState(fetchData())
+        }
+    }, [keyName_])
+
+    useEffect(() => {
+        isMountedRef.current = true
+    }, [])
 
     return [state, setState_]
 }
