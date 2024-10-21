da4f3373e1157fb72f5399b9b59dd9e045effd0a
Mauro <salgadom7503@gmail.com>
2024-10-21 14:00:26 +0100

    n

--- src/routes/--ui/Link.jsx
+++
@@ -1,6 +0,0 @@
-import { cn } from "@/lib/utils"
-import { Link as LinkTS } from "@tanstack/react-router"
-
-const Link = props => <LinkTS className={cn("[&.active]:font-bold", props.className)} {...props} />
-
-export default Link
